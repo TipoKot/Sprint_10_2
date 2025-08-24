fc65b1737e0d4b8906488604ee89aea5b83e250e
aakuzmh <tipokot@gmail.com>
2025-08-24 14:25:52 +0300

    Исправил замечания

--- helpers.py
+++ helpers.py
@@ -5,20 +5,28 @@ from pathlib import Path
 
 
 class ApiClient:
+    def __init__(self):
+        self.token = None
+
     def register_new_user(self):
         email = f"user_{uuid.uuid4()}@example.com"
         password = "securePassword123"
         return requests.post(f"{BASE_URL}/signup", json={"email": email, "password": password})
 
     def signin(self, email: str, password: str) -> str:
-        return requests.post(f"{BASE_URL}/signin", json={"email": email, "password": password})
+        response = requests.post(f"{BASE_URL}/signin", json={"email": email, "password": password})
+        self.token = response.json()["token"]["access_token"]
+        return response
+    
+    def get_auth_headers(self):
+        return {"Authorization": f"Bearer {self.token}"}
 
-    def create_listing(self, headers: dict, order_payload: dict):
-        img_path = Path(test_order.get("image_path", "test.png"))
+    def create_listing(self, order_payload: dict):
+        img_path = Path(__file__).resolve().parent / "test.png"
         files = {"images": (img_path.name, open(img_path, "rb"), "image/png")}
-        return requests.post(f"{BASE_URL}/create-listing", data=order_payload, files=files, headers=headers)
+        return requests.post(f"{BASE_URL}/create-listing", data=order_payload, files=files, headers=self.get_auth_headers())
 
-    def update_offer(self, headers: dict, listing_id: int, fields: dict):
-        img_path = Path(test_order.get("image_path", "test.png"))
+    def update_offer(self, listing_id: int, fields: dict):
+        img_path = Path(__file__).resolve().parent / "test.png"
         files = {"images": (img_path.name, open(img_path, "rb"), "image/png")}
-        return requests.patch(f"{BASE_URL}/update-offer/{listing_id}", data=fields, files=files, headers=headers)
+        return requests.patch(f"{BASE_URL}/update-offer/{listing_id}", data=fields, files=files, headers=self.get_auth_headers())
--- tests/test_order.py
+++ tests/test_order.py
@@ -9,10 +9,6 @@ class TestOrder:
     def test_create_order(self):
         apiclient = ApiClient()
         auth_response = apiclient.signin(test_user["email"], test_user["password"])
-        assert auth_response.status_code == 201
-        
-        auth_data = auth_response.json()
-        token = auth_data["token"]["access_token"]
 
         # Создание объявления
         order_payload = {
@@ -22,13 +18,9 @@ class TestOrder:
             "city": test_order["city"],
             "description": test_order["description"],
             "price": str(test_order["price"]),
-            "image": test_order["image"]
         }
 
-        headers = {
-            "Authorization": f"Bearer {token}"
-        }
-        order_response = apiclient.create_listing(headers, order_payload)
+        order_response = apiclient.create_listing(order_payload)
         assert order_response.status_code == 201
         order_data = order_response.json()
 
@@ -43,10 +35,6 @@ class TestOrder:
         # Авторизация пользователя
         apiclient = ApiClient()
         auth_response = apiclient.signin(test_user["email"], test_user["password"])
-        assert auth_response.status_code == 201
-
-        auth_data = auth_response.json()
-        token = auth_data["token"]["access_token"]
 
         # Создание объявления для редактирования
         order_payload = {
@@ -58,10 +46,7 @@ class TestOrder:
             "price": str(test_order["price"]),
         }
 
-        headers = {
-            "Authorization": f"Bearer {token}"
-        }
-        order_response = apiclient.create_listing(headers, order_payload)
+        order_response = apiclient.create_listing(order_payload)
         assert order_response.status_code == 201
         order_data = order_response.json()
         order_id = order_data["id"]
@@ -77,7 +62,7 @@ class TestOrder:
         }
         img_path = Path(test_order.get("image_path", "test.png"))
         files = {"images": (img_path.name, open(img_path, "rb"), "image/png")}
-        edit_response = apiclient.update_offer(headers, order_id, edited_order_payload)
+        edit_response = apiclient.update_offer(order_id, edited_order_payload)
         assert edit_response.status_code == 200
         edit_data = edit_response.json()
 
@@ -88,10 +73,6 @@ class TestOrder:
         # Авторизация пользователя
         apiclient = ApiClient()
         auth_response = apiclient.signin(test_user["email"], test_user["password"])
-        assert auth_response.status_code == 201
-
-        auth_data = auth_response.json()
-        token = auth_data["token"]["access_token"]
 
         edited_order_payload = {
             "name": "Updated Test Order",
@@ -103,10 +84,7 @@ class TestOrder:
             "image": test_order["image"]
         }
 
-        headers = {
-            "Authorization": f"Bearer {token}"
-        }
-        edit_response = apiclient.update_offer(headers, 68, edited_order_payload)
+        edit_response = apiclient.update_offer(68, edited_order_payload)
         
         # Ожидается ошибка доступа или несуществующего ресурса
         assert edit_response.status_code == 401
@@ -116,10 +94,6 @@ class TestOrder:
         # Авторизация пользователя
         apiclient = ApiClient()
         auth_response = apiclient.signin(test_user["email"], test_user["password"])
-        assert auth_response.status_code == 201
-
-        auth_data = auth_response.json()
-        token = auth_data["token"]["access_token"]
 
         # Создание объявления для удаления
         order_payload = {
@@ -132,16 +106,13 @@ class TestOrder:
             "image": test_order["image"]
         }
 
-        headers = {
-            "Authorization": f"Bearer {token}"
-        }
-        order_response = apiclient.create_listing(headers, order_payload)
+        order_response = apiclient.create_listing(order_payload)
         assert order_response.status_code == 201
         order_data = order_response.json()
         order_id = order_data["id"]
 
         # Удаление объявления
-        delete_response = requests.delete(f"{BASE_URL}/listings/{order_id}", headers=headers)
+        delete_response = requests.delete(f"{BASE_URL}/listings/{order_id}", headers=apiclient.get_auth_headers())
         assert delete_response.status_code == 200
         delete_data = delete_response.json()
         assert delete_data["message"] == "Объявление удалено успешно"
